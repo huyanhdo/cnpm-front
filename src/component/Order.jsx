19101696c946786b092c428fa85917f40c67a7c5
{PDN NALLOO} <{nghia.pd194129@sis.hust.edu.vn}>
2021-11-11 10:31:27 +0700

    Trending-full

--- src/component/Order.jsx
+++ src/component/Order.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
 import './Order.css'
+import PropTypes from 'prop-types'
 import crown from './img/noto_crown.jpg'
 
-function Order() {
+// Order.PropTypes = {
+//     orders: PropTypes.array,
+// };
+// Order.defaultProps = {
+//     order: [],
+// };
 
-    return (
+function Order(props) {
+    const { orders } = props;
 
-        <div className="card">
-            <div className="card-content">
-                <div className="card-content-top">
-                    <img src={crown} />
-                    Top of the week
+    return (<div className="trending-content">
+        {orders.map(order => (
+            <div className="card" key={order.id}>
+                <div className="card-content">
+                    <div className="card-content-top">
+                        <img src={crown} />
+                        Top of the {order.top}
+                    </div>
+                    <div className="card-title"><h3 className="card-content-title">{order.title}</h3></div>
+                    <div className="card-content-cal">{`${order.cal} Calories`}</div>
+                    <div style={{
+                        width: '31px',
+                        height: '0px',
+                        border: '1px solid #E3E1E1'
+                    }}></div>
+                    <div className="card-content-person">{`${order.person} person`}</div>
+                    <div className="card-content-price">
+                        <div className="price">{`$ ${order.price}`}</div>
+                        <button className="add"> + </button>
+                    </div>
+
+
                 </div>
-                <div className="card-title"><h3 className="card-content-title">Pizza</h3></div>
-                <div className="card-content-cal">100 Calories</div>
-                <div className="card-content-person">5 person</div>
-                <div className="card-content-cost">
-                    <div className="cost">$ 7.49</div>
-                    <button className="add"> + </button>
-                </div>
+                <img id="thumbnail1" src={order.img} alt="" />
 
-
-            </div>
-            <img id="thumbnail1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU" alt="" />
-
-        </div>
+            </div>))}
+    </div>
     );
 }
 
--- src/component/TrendingOrder.jsx
+++ src/component/TrendingOrder.jsx
@@ -5,29 +5,126 @@ import './TrendingOrder.css'
 
 
 function TrendingOrder() {
+    const [trendingorderList, setTrendingorderList] = useState([
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
 
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+        {
+            id: 1,
+            top: "week",
+            cal: 500,
+            title: "pizza",
+            person: 5,
+            price: 7.49,
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6BiEad3ASAlthVZG5WvRPFZxolEvUlmA4w&usqp=CAU'
+        },
+    ])
 
     return (
         <div className="TrendingOrder">
 
             <div className="Trending-title">
-                <div className="Trending-top">
-                    <h2>Trending Orders</h2>
-                    <div className="view-all">
-                        <p>View all <i class="fas fa-chevron-circle-right"></i></p>
 
-                    </div>
+                <h2>Trending Orders</h2>
+                <div className="view-all">
+                    <p>View all <i class="fas fa-chevron-circle-right"></i></p>
+
                 </div>
+
             </div>
 
             <div className="cards">
+                <Order orders={trendingorderList} />
+                {/* <Order />
+                <Order />
+                <Order />
+                <Order />
+                <Order />
+                <Order /> */}
 
-                <Order />
-                <Order />
-                <Order />
-                <Order />
-                <Order />
-                <Order />
             </div>
 
 
